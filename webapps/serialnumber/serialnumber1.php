4041ea5106d340da413b17c9ab32abb712ffbde1
yuya_ishino <ishino-yuya@mercury-soft.co.jp>
2020-10-13 22:00:53 +0900

    シリアルナンバー入力注釈追加

--- webapps/serialnumber/serialnumber1.php
+++ webapps/serialnumber/serialnumber1.php
@@ -233,6 +233,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     </div>
                 </form>
                 <div class="point_block error" style="display: none">エラー表示</div>
+                <div class="point_block check" style="margin: 0 auto 0px">半角数字16桁の入力をお願い致します。</div>
                 <div class="point_block check">3回連続して入力エラーとなった場合、本人認証が必要となります。</div>
                 <div class="btn-list-area">
                     <a class="btn-link btn btn-yel" onclick="submit()">登録</a>
